fc64859e420ba0ab47783f8a6382b405d1358c5a
smileywrld <ismaileyyunusa@gmail.com>
2026-03-06 09:44:14 +0100

    feat: update Hero component layout for better responsiveness and enhance Navbar with sticky positioning

--- main-project/src/components/Hero.jsx
+++ main-project/src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
 	return (
-		<div className="container w-full px-5 py-4 flex flex-col gap-9 items-center justify-center h-full">
-			<div className="container w- px-5 py-4 flex flex-col gap-9 items-center justify-center ">
+		<div className="w-full flex items-center justify-center h-screen">
+			<div className="container px-5 py-4 flex flex-col gap-9 items-center justify-center">
 				<h2 className="text-5xl text-center  font-bold">Blog & Article</h2>
 				<p className="text-center text-2xl">
 					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
@@ -16,7 +16,7 @@ const Hero = () => {
 						placeholder="search article, news or recipe"
 						className="w-80"
 					/>
-					<button className="bg-black text-white border p-5 rounded-2xl w-40 m-2">
+					<button className="cursor-pointer bg-black text-white border p-5 rounded-2xl w-40 m-2">
 						Search
 					</button>
 				</div>

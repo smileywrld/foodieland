914170a9e765d1cd35757fc454b06651f646d35c
smileywrld <ismaileyyunusa@gmail.com>
2026-03-06 09:36:28 +0100

    feat: add Hero component for blog and article section

--- main-project/src/App.jsx
+++ main-project/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Navbar from "./components/navbar";
+import Hero from "./components/Hero";
 import "./App.css";
 
 function App() {
 	return (
 		<main>
 			<Navbar />
+			<Hero />
 		</main>
 	);
 }

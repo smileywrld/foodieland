371af3948cd52f9b98916e04efd91545cbf5ccc5
smileywrld <ismaileyyunusa@gmail.com>
2026-03-06 07:35:07 +0100

    feat: enhance Navbar component with updated layout, styling, and social media icons

--- main-project/src/components/navbar.jsx
+++ main-project/src/components/navbar.jsx
@@ -1,45 +1,41 @@
 const Navbar = () => {
 	return (
-		<nav class="bg-white shadow shadow-gray-300 w-full px-8 md:px-auto">
-			<div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
-				<div class="md:order-1">
+		<nav className="bg-white shadow shadow-gray-300 w-full px-8 md:px-auto">
+			<div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
+				<div className="md:order-1">
 					<h1 className="text-2xl font-bold italic">FoodieLand</h1>
 				</div>
-				<div class="text-gray-500 order-3 w-full md:w-auto md:order-2">
-					<ul class="flex font-semibold justify-between">
-						<li class="md:px-4 md:py-2 text-indigo-500">
-							<a href="#">Dashboard</a>
+				<div className="text-gray-500 order-3 w-full md:w-auto md:order-2">
+					<ul className="flex font-semibold justify-between">
+						<li className="md:px-4 md:py-2">
+							<a href="#">Home</a>
 						</li>
-						<li class="md:px-4 md:py-2 hover:text-indigo-400">
-							<a href="#">Search</a>
+						<li className="md:px-4 md:py-2 ">
+							<a href="#">Recipe</a>
 						</li>
-						<li class="md:px-4 md:py-2 hover:text-indigo-400">
-							<a href="#">Explore</a>
+						<li className="md:px-4 md:py-2 ">
+							<a href="#">Blog</a>
 						</li>
-						<li class="md:px-4 md:py-2 hover:text-indigo-400">
-							<a href="#">About</a>
+						<li className="md:px-4 md:py-2 ">
+							<a href="#">About us</a>
 						</li>
-						<li class="md:px-4 md:py-2 hover:text-indigo-400">
+						<li className="md:px-4 md:py-2 ">
 							<a href="#">Contact</a>
 						</li>
 					</ul>
 				</div>
-				<div class="order-2 md:order-3">
-					<button class="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="h-5 w-5"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
-								clip-rule="evenodd"
-							/>
-						</svg>
-						<span>Login</span>
-					</button>
+				<div className="order-2 md:order-3 flex gap-4 text-2xl">
+					<a href="#" className="hover:text-blue-600">
+						<ion-icon name="logo-facebook"></ion-icon>
+					</a>
+
+					<a href="#" className="hover:text-black">
+						<ion-icon name="logo-twitter"></ion-icon>
+					</a>
+
+					<a href="#" className="hover:text-pink-600">
+						<ion-icon name="logo-instagram"></ion-icon>
+					</a>
 				</div>
 			</div>
 		</nav>

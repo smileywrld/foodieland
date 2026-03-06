73f7982be5553026aaca5444c85832c31fd7eca2
smileywrld <ismaileyyunusa@gmail.com>
2026-03-06 07:01:40 +0100

    feat: initialize main project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, and linting.
    - Created Vite configuration file for React and Tailwind CSS integration.
    - Set up main application structure with App component, Navbar component, and CSS imports.
    - Included SVG assets for Vite and React logos.
    - Established basic styling with Tailwind CSS in App.css and index.css.

---
+++ main-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Navbar from "./components/navbar";
+import "./App.css";
+
+function App() {
+	return (
+		<main>
+			<Navbar />
+		</main>
+	);
+}
+
+export default App;
---
+++ main-project/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+	return (
+		<div className="sticky bg-white top-0 z-10">
+			<div className="container hidden lg:block">
+				<div className="flex justify-between items-center p-8">
+					<h1 className="text-3xl font-medium">FoodieLand</h1>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
